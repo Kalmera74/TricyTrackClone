0b980fc3a808828f88e2a5ec2fee8305f0a24444
kalmera74 <kalmera74@gmail.com>
2021-05-20 22:51:58 +0300

    improved shooter, added player controler, main scene

--- Assets/Script/Shooter.cs
+++ Assets/Script/Shooter.cs
@@ -6,110 +6,174 @@ public class Shooter : MonoBehaviour
 {
     // Ball object that the player avatar throw
     [SerializeField]
-    private GameObject Ball;
+    private GameObject _ball;
     // Start position of the trajectory which is the player position
     [SerializeField]
-    private Vector3 StartingPossition;
+    private Vector3 _startingPosition;
     // The initial velocity of the ball that is used for calculating the trajectory
     [SerializeField]
-    private Vector3 InitialVelocity;
+    private Vector3 _initialVelocity;
 
     // Determines the smoothness of the trajectory line. THe higher the count the smoother the line
     [SerializeField]
-    private int Resolution = 10;
+    private int _resolution = 10;
 
     // length of the trajectory line
     [SerializeField]
-    private int Length = 10;
+    private int _length = 10;
 
     // The gravity constant used for calculating the trajectory and used by all the physic objects
     [SerializeField]
-    private float Gravity = 5f;
+    private float _gravity = 5f;
 
     // Line renderer object that draws the trajectory
     [SerializeField]
-    private LineRenderer LineRender;
+    private LineRenderer _lineRenderer;
 
+    [SerializeField]
+    private float _minSwipeLength = 200f;
+    private Vector2 _firstPressPos;
+    private Vector2 _secondPressPos;
+    private Vector2 _currentSwipe;
+    private enum Swipe { None, Up, Down, Left, Right };
+
+    private static Swipe swipeDirection;
     void Awake()
     {
         // Set the global gravity to the Gravity variable
-        Physics.gravity = new Vector3(0, -Gravity, 0);
+        Physics.gravity = new Vector3(0, -_gravity, 0);
     }
 
     // Update is called once per frame
     void Update()
     {
-        // Set the StartingPossition to the current position of the player
-        StartingPossition = gameObject.transform.position;
-
-        //! For testing
-        if (Input.GetKey(KeyCode.LeftArrow))
+        if (Input.touches.Length > 0)
         {
-            InitialVelocity += Vector3.left * 1.1f;
+            Touch t = Input.GetTouch(0);
+
+            if (t.phase == TouchPhase.Began)
+            {
+                _firstPressPos = new Vector2(t.position.x, t.position.y);
+            }
+
+
+            if (t.phase == TouchPhase.Ended || t.phase == TouchPhase.Moved)
+            {
+                _secondPressPos = new Vector2(t.position.x, t.position.y);
+                _currentSwipe = new Vector3(_secondPressPos.x - _firstPressPos.x, _secondPressPos.y - _firstPressPos.y);
+
+                // Make sure it was a legit swipe, not a tap
+                if (_currentSwipe.magnitude < _minSwipeLength)
+                {
+                    swipeDirection = Swipe.None;
+                    return;
+                }
+
+                _currentSwipe.Normalize();
+
+                if (_currentSwipe.y > 0 && _currentSwipe.x > -0.5f && _currentSwipe.x < 0.5f)
+                {
+                    swipeDirection = Swipe.Down;
+                }
+                else if (_currentSwipe.y < 0 && _currentSwipe.x > -0.5f && _currentSwipe.x < 0.5f)
+                {
+                    swipeDirection = Swipe.Up;
+                }
+                else if (_currentSwipe.x < 0 && _currentSwipe.y > -0.5f && _currentSwipe.y < 0.5f)
+                {
+                    swipeDirection = Swipe.Left;
+                }
+                else if (_currentSwipe.x > 0 && _currentSwipe.y > -0.5f && _currentSwipe.y < 0.5f)
+                {
+                    swipeDirection = Swipe.Right;
+                }
+
+                if (t.phase == TouchPhase.Ended)
+                {
+                    Shoot();
+                }
+            }
+        }
+        else
+        {
+            swipeDirection = Swipe.None;
+
         }
 
-        if (Input.GetKey(KeyCode.RightArrow))
+        if (swipeDirection == Swipe.Left)
         {
-            InitialVelocity += Vector3.right * 1.1f;
+            _initialVelocity += Vector3.left * 1.1f;
         }
 
-        if (Input.GetKey(KeyCode.UpArrow) && (InitialVelocity.y <= 80f || InitialVelocity.z <= 120f))
+        if (swipeDirection == Swipe.Right)
         {
-            InitialVelocity += Vector3.forward * 1.6f;
-            InitialVelocity += Vector3.up * 1f;
+            _initialVelocity += Vector3.right * 1.1f;
+        }
+
+        if (swipeDirection == Swipe.Up && (_initialVelocity.y <= 80f || _initialVelocity.z <= 120f))
+        {
+            _initialVelocity += Vector3.forward * 1.6f;
+            _initialVelocity += Vector3.up * 1f;
             //TODO: Increase the length according to z and y
             //! Length max 20
-            if (InitialVelocity.y % 9 == 0)
-                Length += 2;
+            if (_initialVelocity.y % 9 == 0 && _length <= 20)
+            {
+                _length += 2;
+            }
 
         }
 
-        if (Input.GetKey(KeyCode.DownArrow))
+        if (swipeDirection == Swipe.Down && (_initialVelocity.y >= 0 || _initialVelocity.z >= 0))
         {
-            InitialVelocity += Vector3.back * 1.6f;
-            InitialVelocity += Vector3.down * 1f;
+            _initialVelocity += Vector3.back * 1.6f;
+            _initialVelocity += Vector3.down * 1f;
 
-            if (InitialVelocity.y % 9 == 0)
-                Length -= 2;
+            if (_initialVelocity.y % 9 == 0 && _length > 0)
+            {
+                _length -= 2;
+            }
 
         }
-
-        if (Input.GetKey(KeyCode.C))
-        {
-            Length += 1;
-        }
-
-
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            GameObject obj = Instantiate(Ball, StartingPossition, Quaternion.identity);
-            obj.GetComponent<Rigidbody>().AddForce(InitialVelocity, ForceMode.Impulse);
-        }
-        //! For testing
 
         Trajectory();
 
+    }
+
+    private void ResetPath()
+    {
+        _lineRenderer.positionCount = 0;
+        _initialVelocity = Vector3.zero;
+        _length = 2;
 
     }
 
+    private void Shoot()
+    {
+
+        GameObject obj = Instantiate(_ball, _startingPosition, Quaternion.identity);
+        obj.GetComponent<Rigidbody>().AddForce(_initialVelocity, ForceMode.Impulse);
+        ResetPath();
+        Destroy(obj, 5f);
+
+    }
     private void Trajectory()
     {
         // Set the vertices count of the linerenderer to the length of the path
-        LineRender.positionCount = Length;
-        Vector3 prevPos = StartingPossition;
-        float ty = InitialVelocity.y;
+        _lineRenderer.positionCount = _length;
+        Vector3 prevPos = _startingPosition;
+        float ty = _initialVelocity.y;
 
-        for (int i = 0; i < Length; i++)
+        for (int i = 0; i < _length; i++)
         {
             // Calculate the trajectory of the Ball, by diving the result by Resolution we get a smooth curves
-            Vector3 nextPos = prevPos + Vector3.up * (ty / Resolution);
-            nextPos.x += (InitialVelocity.x / Resolution);
-            nextPos.z += (InitialVelocity.z / Resolution);
+            Vector3 nextPos = prevPos + Vector3.up * (ty / _resolution);
+            nextPos.x += (_initialVelocity.x / _resolution);
+            nextPos.z += (_initialVelocity.z / _resolution);
             // Set the position of the vertices of the linerenderer
-            LineRender.SetPosition(i, prevPos);
+            _lineRenderer.SetPosition(i, prevPos);
             prevPos = nextPos;
             // Apply gravity
-            ty -= Gravity;
+            ty -= _gravity;
 
         }
     }
